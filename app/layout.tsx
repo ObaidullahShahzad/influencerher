5b6727f57865513147a954f9dba3c54ad9484f9c
ObaidullahShahzad <obaidullah.shahzad@stixor.com>
2025-05-30 21:41:21 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/faveicon.svg" />
+      </head>
       <body>{children}</body>
     </html>
   );
